a8353db13b0811566d8fc9e1054469234b40a19b
Denis <deniska64@ya.ru>
2024-01-26 14:13:41 +0300

    Google style applied

--- guess_number/high_scores.h
+++ guess_number/high_scores.h
@@ -2,9 +2,8 @@
 #include <string>
 #include <vector>
 
-namespace high_score
-{
-    int read_high_scores_from_file();
-    int save_score(const std::string &name, const int attempt);
-    void print_high_scores();
-}
+namespace high_score {
+int read_high_scores_from_file();
+int save_score(const std::string &name, const int attempt);
+void print_high_scores();
+}  // namespace high_score
